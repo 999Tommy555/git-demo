68c5c8957b68174fc2b21f0a64b5912cdafa53fd
Tommy <Tommy@gmail.com>
2025-07-18 20:09:50 +0800

    更改3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,20 @@
-a=eval(input("請輸入a:"))
-b=eval(input("請輸入b:"))
-if a>b:
-    print(f"a比b大{a-b}")
-elif b>a:
-    print(f"b比a大{b-a}")
-else:
-    print("a跟b一樣大")
+
+while True:
+    try:
+        a=input("請輸入a(exit:離開)")
+        if a=="exit":
+            break
+        a=eval(a)
+        b=eval(input("請輸入b:"))
+        if a>b:
+            print(f"a比b大{a-b}")
+        elif b>a:
+            print(f"b比a大{b-a}")
+        else:
+            print("a跟b一樣大")
+        
+        if input("leave?(y/n)")=="y":
+            break
+
+    except Exception as e:
+        print("輸入錯誤!",e)
